458669ea5b6201957d50910e67dce13f04c18f7c
jaspreet kaur <kaurjas1928@yahoo.com>
2023-05-18 19:03:00 +0100

    Fifth test - paper vs. rock

--- index.test.js
+++ index.test.js
@@ -1,7 +1,9 @@
 function rps(left, right) {
-    return left === "rock" || right === "paper"
-    ? "left"
-    : "right";
+    return left === "rock" ||
+      right === "paper" ||
+      (left === "paper" && right === "rock")
+      ? "left"
+      : "right";
 }
 describe("rock, paper, scissors", () => {
   it("should say right wins for rock vs. scissors", () => {
@@ -38,5 +40,14 @@ describe("rock, paper, scissors", () => {
     const result = rps(left, right);
 
     expect(result).toBe("right");
+  });
+
+  it("should say left wins for paper vs. rock", () => {
+    const left = "paper";
+    const right = "rock";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("left");
   });
 });
