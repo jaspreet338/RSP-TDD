233714d6669b09ddec6347e2e78bed21f96bf4bf
jaspreet kaur <kaurjas1928@yahoo.com>
2023-05-18 18:52:02 +0100

    Second test - scissors vs. rock

--- index.test.js
+++ index.test.js
@@ -1,7 +1,16 @@
-function rps() {
-     return "left";
+function rps(left) {
+    return left === "rock" ? "left" : "right";
 }
 describe("rock, paper, scissors", () => {
+  it("should say right wins for rock vs. scissors", () => {
+    const left = "scissors";
+    const right = "rock";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
+  });
+
   it("should say left wins for rock vs. scissors", () => {
     const left = "rock";
     const right = "scissors";
